9614a67b964af53040abdc0375a99adb2c9b30c3
stellapavlidou <116185753+stellapavlidou@users.noreply.github.com>
2024-11-01 01:50:55 +0200

    Add files via upload

--- MicroLab/main.asm
+++ MicroLab/main.asm
@@ -1,355 +1,167 @@
 ;
-; lab4 - ex1.asm
+; Omada3_1.asm
 ;
-; Author : user
+; Created: 23/10/2024 10:19:13 πμ
+; Author : pavli
 ;
-.include "m328PBdef.inc" 
-.org 0x00
+
+;
+; lab3 - ex1.asm
+;
+; Author : KG
+;
+
+;ΣΗΜΕΙΩΣΕΙΣ:> Εχω χρησιμοποισει τους καταχωρητες ρ16-ρ20 και τους ρ27,28(Χ) και ρ30,31(Ζ)
+;           > πιθανον να υπαρχει προβλημα με τους σπινθηρισμους
+;           > εβαλα το Fast PWM, 8-bit ελπιζω αυτο να ζηταει
+
+.include "m328PBdef.inc"
+.equ MU=150
+.org 0x0
 rjmp reset
 
-.org 0x1A
-rjmp TMR1_ISR
+.def DC_VALUE=r16
+.def offset=r17
 
-.org 0x2A ;ADC Conversion Complete Interrupt
-rjmp ADC_ISR
-
-; Define constants for TMR1
-.equ PRESCALER = 256
-.equ TIMER1_COMPARE_VALUE = 62499
-.equ PD2=2
-.equ PD3=3
-; Register definitions
-.def resultL = r26           ; Lower byte of the result (in mV)
-.def resultH = r27           ; Higher byte of the result (in mV)
-
-.def temp = r16
-.def ADC_L = r21
-.def ADC_H = r22
-
-.def hundreds=r29
-.def tens=r30
-.def units=r31
-
+;======================================== RESET / INITIALIZE
 reset:
-ldi r24, low(RAMEND)
+ldi r24, LOW(RAMEND)
 out SPL, r24
-ldi r24, high(RAMEND)
-out SPH, r24 ; init stack pointer
+ldi r24, HIGH(RAMEND)
+out SPH, r24
 
-ser r24
-out DDRD, r24 ; set PORTD as output
+ldi DC_VALUE,0x06; starting value 50% , οπου για να βρω το ποσοστο κανω 
+                 ;  DC_VALUE * 8 +2 και αρα παιρει τιμες απο 0 - 12
+
+SER r24
+out DDRB,r24 ; Init PORTB as output
+
 clr r24
-out DDRC, r24; set PINC as input
-
-rcall lcd_init
-ldi r24, low(100)
-ldi r25, high(100) ; delay 100 mS
-rcall wait_msec
-
-rcall init_adc
-rcall init_tmr1
-
-sei
+out DDRD,r24  ; PIND as input            USE PIND FOR INPUT!!!!!!!!
 
 
+;PWM set up
+ldi r24, (1<<WGM10)| (1 << WGM11) | (1<<COM1A1) ; COM1A1 Συνδεση με pb1
+sts TCCR1A, r24                   ; CS 100 CLK/256 = 62500 Hz
+ldi r24, (1<<WGM12) | (1<<CS12)   ; WGM 0101 = Fast PWM, 8-bit 
+sts TCCR1B, r24 
+ 
+
+; registers X hold the start of the array with the precalc values for OCR1A
+ldi XL, low(2*array)          ; Z register low byte (index)
+ldi XH, high(2*array)         ; Z register high byte (address base)
+
+
+;============================================ MAIN 
 main:
-nop
-ldi r24, low(100)
-ldi r25, high(100) ; delay 100 mS
-rcall wait_msec 
-jmp main
+sbis PIND,3
+inc DC_VALUE
+
+sbis PIND,4
+dec DC_VALUE
+
+rcall wait_x_msec 
+
+movw Z , X
+; Add the offset to ZL 
+mov offset, DC_VALUE
+lsl offset ; offset * 2
+add ZL, offset   
+brcc no_carry     ; Branch if no carry (addition didn't overflow)
+
+; If there was a carry, increment ZH 
+inc ZH
+
+no_carry:
+lpm r18,Z+  ;low byte
+lpm r19,Z   ;high byte
+
+sts OCR1AL, R18        ; βαζω στον ocr1a το DC σε 16bit μορφη
+sts OCR1AH, R19
+
+cpi DC_VALUE, 0x0C    ;ελεγξε οτι το DC εχει φτασει στο MAX  (12dec)         
+breq maxed
+
+cpi DC_VALUE, 0x00    ; ελεγξε οτι το DC εχει φτασει στο MΙΝ
+breq minim
+
+rjmp main
 
 
+;=============================
+; Aν ειμαστε στο μεγιστο ή στο ελαχιστο
+maxed:
+sbis PIND,4
+rjmp endmax
 
-;=======================================================
-calc_voltage:
-; Mπορω να θυσιασω 1 bit  του adc, μιας και 10bit δινουν ακριβεια 1/1024 * 5 = 0,004
-; που δεν μας χρειαζετε αφου στρογγυλοποιουμε στο δευτερο δεκαδικο και με μονο 9bit
-; εχουμε ακριβεια 1/512 * 5 = 0,009. Συνεπως,
-; Vin= ADC/512 * 500 = ADC/128 * 125 με 0<=ADC<=512 (125 * 512 = 64000 <65535 αρα το πολυ 16bit)
+rjmp maxed
 
-; Step 1: Load 125 into temp registers
-ldi temp, low(125)     ; Load lower byte 
+endmax:
+dec DC_VALUE
+rjmp main
 
-clc
-ror ADC_H
-ROR ADC_L
-clc
-; Step 2: Multiply ADC * 125 με 0<=ADC<=512 (125 * 512 =64000 <65535 αρα το πολυ 16bit)
-mul ADC_L, temp          ; Multiply lower bytes, result in r0:r1
-mov resultL, r0          ; Move low byte to resultL
-mov resultH, r1          ; Move high byte to resultH
+;====
+minim:
+sbis PIND,3
+rjmp endmin
 
-mul ADC_H, temp          ; Multiply ADC_H by tempL
-add resultH, r0          ; Add to result high byte
-clr r0                   ; Clear r0 after use
+rjmp minim
 
-; Step 3: Divide the result by 128 (shift 7 to right)
-ldi temp,7
-div: 
-clc                        ; clear carry
-ror  resultH               ; Shift (with carry)
-ror  resultL               ; Shift 
-dec temp
-brne div
+endmin:
+inc DC_VALUE
+rjmp main
 
-clr hundreds
-clr tens
-clr units
+wait_x_msec:
+push r23
 
-; find hundrends digit
-calcHundreds:
-cpi resultL, 100
-sbrs resultH,0
-brlo calcTens
-sbiw resultL, 63
-sbiw resultL, 37
-inc hundreds
-rjmp calcHundreds
+in r23, SREG
+push r23 ; save 23, SREG to stack
+push r22
 
-calcTens:       ; at this point the value is only in resultL
-cpi resultL,10
-brlo calcUnits
-subi resultL,10
-inc tens
-rjmp calcTens
+ ldi r24, low(MU)
+ ldi r25, high(MU)
+;==============
 
-calcUnits:
-mov units,resultL
+delay1:
+ldi r22, 0x10 ;r22=16
 
-ret                     ; Return with result in resultL:resultH 
+delay3:
+ldi r23, 235 ; (1 cycle)
 
-
-;========================================================
-init_adc:
-;//REFSn[1:0]=01 => select Vref=5V, MUXn[4:0]=0000 => select ADC1(pin PC1),
-;// ADLAR=0 =>right adjust the ADC result
-ldi temp, 0b01000001 ;
-sts ADMUX, temp
-;// ADEN=1 => ADC Enable, ADCS=0 => No Conversion,
-;// ADIE=1 => Enable adc interrupt, ADPS[2:0]=111 => fADC=16MHz/128=125KHz
-ldi temp, 0b10001111
-sts ADCSRA, temp
-ret
-
-;=======================================================
-init_tmr1:
-; Set up Timer1
-; Set CTC mode (WGM12 = 1), Set prescaler to 256 (CS12 = 1) 
-ldi r16, (1 << WGM12) | (1 << CS12) 
-sts TCCR1B, r16
-
-; Set the compare value
-ldi r16, low(TIMER1_COMPARE_VALUE)
-sts OCR1AL, r16       ; Set lower byte
-ldi r16, high(TIMER1_COMPARE_VALUE)
-sts OCR1AH, r16       ; Set upper byte
-
-
-; Enable Timer1 Compare Match Interrupt
-ldi r16, (1 << OCIE1A) ; so tha it triggers when it reaches the value stored in OCR1A
-sts TIMSK1, r16        ; Enable Timer1 compare match interrupt
-
-ret
-
-;=======================================================
-TMR1_ISR:
-lds temp, ADCSRA    ;
-ori temp, (1<<ADSC) ; Set ADSC flag of ADCSRA
-sts ADCSRA, temp    ; it start ADC every 1sec  
-reti
-
-
-;=======================================================
-ADC_ISR:
-lds ADC_L,ADCL ; Read ADC result
-lds ADC_H,ADCH ;
-
-rcall lcd_clear_display
-rcall calc_voltage
-
-ldi r24,'V'
-call lcd_data
-ldi r24,'O'
-call lcd_data
-ldi r24,'L'
-call lcd_data
-ldi r24,'T'
-call lcd_data
-ldi r24,'A'
-call lcd_data
-ldi r24,'G'
-call lcd_data
-ldi r24,'E'
-call lcd_data
-ldi r24,':'
-call lcd_data
-ldi r24,' '
-call lcd_data
-
-ldi r24,'0'
-add r24, hundreds
-call lcd_data
-
-ldi r24,','
-call lcd_data
-
-ldi r24,'0'
-add r24, tens
-call lcd_data
-
-ldi r24,'0'
-add r24, units
-call lcd_data
-
-ldi r24,'V'
-call lcd_data
-
-ldi r24, low(200)
-ldi r25, high(200) ; delay 100 mS
-rcall wait_msec 
-
-reti
-
-;======================================================
-
-;======================================================================
-;=====================  COPY απο διαφανειες  ============================
-;======================================================================
-;Αρχικοποίηση και ρυθμίσεις της οθόνης LCD όπως παρουσιάζεται παρακάτω:
-;DL = 0 4 bit mode
-;N = 1 2 lines
-;F = 0 5×8 dots
-;D = 1 display on
-;C = 0 cursor off
-;B = 0 blinking off
-;I/D = 1 DD-RAM address auto increment
-;SH = 0 shift of entire display off
-
-lcd_init:
-ldi r24 ,low(200) ;
-ldi r25 ,high(200) ; Wait 200 mSec
-rcall wait_msec ;
-ldi r24 ,0x30 ; command to switch to 8 bit mode
-out PORTD ,r24 ;
-sbi PORTD ,PD3 ; Enable Pulse
-nop
-nop
-cbi PORTD ,PD3
-ldi r24 ,250 ;
-ldi r25 ,0 ; Wait 250uSec
-rcall wait_usec ;
-ldi r24 ,0x30 ; command to switch to 8 bit mode
-out PORTD ,r24 ;
-sbi PORTD ,PD3 ; Enable Pulse
-nop
-nop
-cbi PORTD ,PD3
-ldi r24 ,250 ;
-ldi r25 ,0 ; Wait 250uSec
-rcall wait_usec ;
-ldi r24 ,0x30 ; command to switch to 8 bit mode
-out PORTD ,r24 ;
-sbi PORTD ,PD3 ; Enable Pulse
-nop
-nop
-cbi PORTD ,PD3
-ldi r24 ,250 ;
-ldi r25 ,0 ; Wait 250uSec
-rcall wait_usec
-ldi r24 ,0x20 ; command to switch to 4 bit mode
-out PORTD ,r24
-sbi PORTD ,PD3 ; Enable Pulse
-nop
-nop
-cbi PORTD ,PD3
-ldi r24 ,250 ;
-ldi r25 ,0 ; Wait 250uSec
-rcall wait_usec
-ldi r24 ,0x28 ; 5x8 dots, 2 lines
-rcall lcd_command
-ldi r24 ,0x0c ; dislay on, cursor off
-rcall lcd_command
-rcall lcd_clear_display
-ldi r24 ,0x06 ; Increase address, no display shift
-rcall lcd_command ;
-ret
-
-;=======================================================
-lcd_clear_display:
-ldi r24 ,0x01 ; clear display command
-rcall lcd_command
-ldi r24 ,low(5) ;
-ldi r25 ,high(5) ; Wait 5 mSec
-rcall wait_msec ;
-ret	
-
-
-;=======================================================
-lcd_command:
-cbi PORTD ,PD2 ; LCD_RS=0(PD2=0), Instruction
-rcall write_2_nibbles ; send Instruction
-ldi r24 ,250 ;
-ldi r25 ,0 ; Wait 250uSec
-rcall wait_usec
-ret
-
-;=======================================================
-lcd_data:
-sbi PORTD ,PD2 ; LCD_RS=1(PD2=1), Data
-rcall write_2_nibbles ; send data
-ldi r24 ,250 ;
-ldi r25 ,0 ; Wait 250uSec
-rcall wait_usec
-ret
-
-;======================================================
-write_2_nibbles:
-push r24 ; save r24(LCD_Data)
-in r25 ,PIND ; read PIND
-andi r25 ,0x0f ;
-andi r24 ,0xf0 ; r24[3:0] Holds previus PORTD[3:0]
-add r24 ,r25 ; r24[7:4] <-- LCD_Data_High_Byte
-out PORTD ,r24 ;
-sbi PORTD ,PD3 ; Enable Pulse
-nop
-nop
-cbi PORTD ,PD3
-pop r24 ; Recover r24(LCD_Data)
-swap r24 ;
-andi r24 ,0xf0 ; r24[3:0] Holds previus PORTD[3:0]
-add r24 ,r25 ; r24[7:4] <-- LCD_Data_Low_Byte
-out PORTD ,r24
-sbi PORTD ,PD3 ; Enable Pulse
-nop
-nop
-cbi PORTD ,PD3
-ret
-
-;===============================================
-wait_msec:
-push r24 ; 2 cycles
-push r25 ; 2 cycles
-ldi r24 , low(999) ; 1 cycle
-ldi r25 , high(999) ; 1 cycle
-rcall wait_usec ; 998.375 usec
-pop r25 ; 2 cycles
-pop r24 ; 2 cycles
+;== ~1ms delay
+delay2:
+dec r23 ; 1 cycle
 nop ; 1 cycle
-nop ; 1 cycle
-sbiw r24 , 1 ; 2 cycles
-brne wait_msec ; 1 or 2 cycles
-ret ; 4 cycles
+brne delay2 ; 1 or 2 cycles
+;==
+
+dec r22
+brne delay3
+
+sbiw r24, 1 ; 2 cycles
+brne delay1 ; 1 or 2 cycles
 
 
-wait_usec:
-sbiw r24 ,1 ; 2 cycles (2/16 usec)
-call delay_8cycles ; 4+8=12 cycles
-brne wait_usec ; 1 or 2 cycles
-ret
+;=============
 
-delay_8cycles:
-nop
-nop
-nop
-ret
+
+; Retrieve r23, SREG from stack
+pop r22
+pop r23
+out SREG, r23
+pop r23 
+
+ret ; Return from call
+
+
+;=============================================
+; Define the matrix with 16-bit values
+array:
+;       2%     10%     18%    26%      34%     42%      50%      58%     66%     74%     82%     90%     98%
+; .dw	20,   102,    184,   266,      348,    430,    512,      594,    676,    758,    840,    922,   1004  
+.dw   0x0014, 0x0066, 0x00b8, 0x010a, 0x015c, 0x01ae,  0x0200, 0x0252,  0x02a4, 0x02f6, 0x0348, 0x039a, 0x03ec
+
+ ;  2%     10%     18%    26%      34%     42%      50%       58%     66%     74%     82%     90%     98%
+; .dw 0x051F, 0x022A, 0x2E14, 0x428F, 0x570A, 0x6B85, 0x8000, 0x947A,	0xA8F6, 0xBD71, 0xD1EB, 0xE666, 0xFAE1
+	  
+		
--- MicroLab/main.c
+++ MicroLab/main.c
@@ -1,5 +1,11 @@
 /*
- *lab3.2
+ * main.c
+ *
+ * Created: 10/24/2024 10:11:26 AM
+ *  Author: pavli
+ */ 
+
+/*
  *  main.c
  *  Author: KG
  */ 
@@ -9,90 +15,91 @@
 #include <util/delay.h>
 
 int main(void){
-	unsigned int duty[13]={
-		
-		20,   // 2%
-		102,  // 10%
-		184,  // 18%
-		266,  // 26%
-		348,  // 34%
-		430,  // 42%
-		512,  // 50%
-		594,  // 58%
-		676,  // 66%
-		758,  // 74%
-		840,  // 82%
-		922,  // 90%
-		1004  // 98%
-	};
+	unsigned int duty[13] = {
+    20,   // 2%
+    102,  // 10%
+    184,  // 18%
+    266,  // 26%
+    348,  // 34%
+    430,  // 42%
+    512,  // 50%
+    594,  // 58%
+    676,  // 66%
+    758,  // 74%
+    840,  // 82%
+    922,  // 90%
+    1004  // 98%
+     };
+	int DC_VALUE = 6 ;  // 0<=DC_VALUE<=12 
+	int mode = 0 ; //  mode 1 = 0,  mode 2 = 1
 	
-	int DC_VALUE = 6 ;           // 0<=DC_VALUE<=12 	
-	unsigned int avg_adc = 0;
-	int count=0;                // count for 16 adc
+	unsigned int ADC_VALUE = 0;
 	
-	// set TMR1 in fast PWM 10bit mode with non-inverted output
+	// set TMR1 in fast PWM 8bit mode with non-inverted output
 	// COM1A1 Συνδεση με pb1
 	// CS 100 CLK/256 = 62500 Hz
-	// WGM 0111 = Fast PWM, 10-bit
+	// WGM 0101 = Fast PWM, 8-bit
 	// prescale = 8
-	TCCR1A = (1<<WGM10) | (1<< WGM11) | (1<<COM1A1);
+	TCCR1A = (1<<WGM10) |(1 << WGM11)| (1<<COM1A1);
 	TCCR1B = (1<<WGM12) | ( 1<<CS12) ;
 	
-	//REFSn[1:0]=01 => select Vref=5V, MUXn[4:0]=0001 => select ADC0(pin PC0),
+	//REFSn[1:0]=01 => select Vref=5V, MUXn[4:0]=0000 => select ADC0(pin PC0),
 	// ADLAR=0 =>right adjust the ADC result
-	ADMUX = 0b010000001 ;
+	ADMUX = 0b01000000 ;
 	
     // ADEN=1 => ADC Enable, ADCS=0 => No Conversion,
     // ADIE=0 => disable adc interrupt, ADPS[2:0]=111 => fADC=16MHz/128=125KHz
     ADCSRA = 0b10000111 ;
 	
 	
-	DDRB = 0xFF;   // B as OUTPUT for PWM
-	DDRC = 0x00;   // C as INPUT for ADC
-	DDRD = 0x1F;   // D as OUTPUT for PD0-PD4 and INPUT for rest PD7 INCR, PD6 DEC 
+	DDRB |= 0xFF;         // B as OUTPUT   ?????????
+	DDRC |= 0xFE;         // C0 as INPUT for POT1
+	DDRD |= 0x00;   // D as INPUT for switches
 	
 	
- while(1){	
-     // Check if PD7 is pressed increase
-     if ((!(PIND & 0b10000000) && DC_VALUE!=12)) DC_VALUE = DC_VALUE + 1;
-     
-     // Check if PD6 is pressed decrease
-     if ((!(PIND & 0b01000000) &&  DC_VALUE!=0)) DC_VALUE = DC_VALUE - 1;
-        						
-      
-      OCR1A = duty[DC_VALUE];  	   
-		    
-	  // Getting 16 measures and using average
-	 
-	 // Set ADSC flag of ADCSRA			
-	 ADCSRA = ADCSRA | (1<<ADSC);
-	  _delay_ms(100); // fixing bounce 
-	  
-	 while ((ADCSRA & (1<<ADSC))) _delay_ms(1); //Wait until ADSC flag gets 0
-	      
-	 avg_adc = avg_adc+ADC;			
-	  count=count+1;
-		    	
-			
-	// Ανάλογα με την τιμή του ADC, ανάβουμε το κατάλληλο LED
-    if (count==16){
-		count=0;
+    while(1){
+		// Check if PD6 is pressed Mode 1
+		if (!(PIND & 0b01000000))  mode = 0;
+ 
+		// Check if PD7 is pressed mode 2
+		if (!(PIND & 0b10000000))  mode = 1;
+        
+		_delay_ms(50); // fixing bounce
 		
-		avg_adc = avg_adc >> 4; // divide by 16		
-    	// PORTD &= ~(0x1F);                  // Σβήνουμε όλα τα LED
-    	 if (avg_adc <= 200) {
-    	     	PORTD = (1 << 0);           // Ανάβουμε το PD0
-    	 } else if (avg_adc <= 400) {
-    	     	PORTD = (1 << 1);           // Ανάβουμε το PD1
-    	 } else if (avg_adc <= 600) {
-    	    	PORTD = (1 << 2);           // Ανάβουμε το PD2
-    	 } else if (avg_adc <= 800) {
-    	    	PORTD = (1 << 3);           // Ανάβουμε το PD3
-    	  } else {
-    	    	PORTD = (1 << 4);           // Ανάβουμε το PD4
-    	 }	
-		avg_adc = 0;			 
-    }
+		
+        if ( mode == 0 ) { // Mode 1 code ( PD1 αυξανει , PD2 μειωνει
+			// Check if PD1 is pressed increase
+			if ((!(PIND & 0b00000010) && DC_VALUE!=12)) DC_VALUE = DC_VALUE + 1;
+
+			// Check if PD7 is pressed decrease
+			if ((!(PIND & 0b00000100) &&  DC_VALUE!=0)) DC_VALUE = DC_VALUE - 1;
+		    						
+		    _delay_ms(50); // fixing bounce
+		 
+		    OCR1A = duty[DC_VALUE];
+
+   			
+			   
+		} else { // Mode 2 code
+			ADC_VALUE = 0; 
+		    
+			// Getting 16 measures and using average
+		    for (int i=0; i<16; i++){
+				// Set ADSC flag of ADCSRA			
+		    	ADCSRA = ADCSRA | (1<<ADSC);
+			
+		    	while ((ADCSRA & (1<<ADSC))) _delay_ms(1); //Wait until ADSC flag gets 0
+			    
+				ADC_VALUE += ADC;			
+		    }
+		    
+			ADC_VALUE = ADC_VALUE >> 4;// divide by 16			
+			// ADC_VALUE = ADC_VALUE << 6 // scale
+			// instead of dividing by 16 and then mult again to reach 16bits in total
+			// i will just use the total sum which in some way is the 16bit average
+			
+			 OCR1A = ADC_VALUE;						 
+        };
 		
     };//while
 	
